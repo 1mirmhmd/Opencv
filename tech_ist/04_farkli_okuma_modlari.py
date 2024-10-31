1d1c7819f40cdae77b8d27fb4a8d2a6f294f0ee6
mirmhmd <mirmhmdmahmud@gmail.com>
2024-11-01 02:01:17 +0300

    Files added

--- tech_ist/04_farkli_okuma_modlari.py
+++ tech_ist/04_farkli_okuma_modlari.py
@@ -7,8 +7,17 @@ img_color=  cv.imread(path,cv.IMREAD_COLOR)
 
 img_gray=  cv.imread(path,cv.IMREAD_GRAYSCALE)
 # img_gray=  cv.imread(path,0)
-cv.imshow("Renkli",img_color)
-cv.imshow("Gri",img_gray)
+# cv.imshow("Renkli",img_color)
+# cv.imshow("Gri",img_gray)
+
+path2="C:/Users/miruz/OneDrive/Belgeler/opencv/tech_ist/yesil_elma.jpg"
+img_color2=  cv.imread(path2,cv.IMREAD_COLOR)
+# img_color2=  cv.imread(path2,1)
+
+img_gray2=  cv.imread(path2,cv.IMREAD_GRAYSCALE)
+# img_gray2=  cv.imread(path2,0)
+cv.imshow("Renkli",img_color2)
+cv.imshow("Gri",img_gray2)
 
 cv.waitKey(0)
-cv.destroyAllWindows          
+cv.destroyAllWindows()  
